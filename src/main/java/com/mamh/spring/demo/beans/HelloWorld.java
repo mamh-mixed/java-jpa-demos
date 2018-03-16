89dd4c3660d8058c475fd812a62e80bc0bddbdf6
Minghui Ma <bright.ma@blackshark.com>
2018-03-16 14:22:05 +0800

    add first spring demo codes
    Signed-off-by: Minghui Ma <bright.ma@blackshark.com>

---
+++ src/main/java/com/mamh/spring/demo/beans/HelloWorld.java
@@ -0,0 +1,33 @@
+package com.mamh.spring.demo.beans;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.web.context.support.XmlWebApplicationContext;
+
+public class HelloWorld {
+    private String name;
+
+    public HelloWorld() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void hello() {
+        System.out.println("hello: = " + getName());
+    }
+
+    public static void main(String[] args) {
+        ClassPathXmlApplicationContext ctx = new ClassPathXmlApplicationContext("applicationContext.xml");
+
+        HelloWorld helloWorld = (HelloWorld) ctx.getBean("helloWorld");
+
+        helloWorld.hello();
+
+    }
+}
