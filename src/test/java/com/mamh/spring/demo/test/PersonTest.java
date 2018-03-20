10bdf7b3d724307c3ef0ff2e1f3c34c1f719f99f
Minghui Ma <bright.ma@blackshark.com>
2018-03-20 11:00:30 +0800

    add abstract bean and parent bean test code
    add abstract bean and parent bean test code
    
    Signed-off-by: Minghui Ma <bright.ma@blackshark.com>

--- src/test/java/com/mamh/spring/demo/test/PersonTest.java
+++ src/test/java/com/mamh/spring/demo/test/PersonTest.java
@@ -10,25 +10,28 @@ public class PersonTest {
     public void test() {
         ClassPathXmlApplicationContext ctx = new ClassPathXmlApplicationContext("applicationContext.xml");
 
-        Person person = (Person) ctx.getBean("person");
-        System.out.println(person);
+//        Person person = (Person) ctx.getBean("person");
+//        System.out.println(person);
+//
+//        Person person1 = (Person) ctx.getBean("person1");
+//        System.out.println(person1);
+//
+//        Person person2 = (Person) ctx.getBean("person2");
+//        System.out.println(person2);
+//
+//        Person person3 = (Person) ctx.getBean("person3");
+//        System.out.println(person3);
+//
+//        Person person4 = (Person) ctx.getBean("person4");
+//        System.out.println(person4);
+//
+//        Person person5 = (Person) ctx.getBean("person5");
+//        System.out.println(person5);
+//
+//        Person person6 = (Person) ctx.getBean("person6");
+//        System.out.println(person6);
 
-        Person person1 = (Person) ctx.getBean("person1");
-        System.out.println(person1);
-
-        Person person2 = (Person) ctx.getBean("person2");
-        System.out.println(person2);
-
-        Person person3 = (Person) ctx.getBean("person3");
-        System.out.println(person3);
-
-        Person person4 = (Person) ctx.getBean("person4");
-        System.out.println(person4);
-
-        Person person5 = (Person) ctx.getBean("person5");
-        System.out.println(person5);
-
-        Person person6 = (Person) ctx.getBean("person6");
-        System.out.println(person6);
+        Person person7 = (Person) ctx.getBean("person7");
+        System.out.println(person7);
     }
 }
