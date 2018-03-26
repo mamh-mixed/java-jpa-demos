8443f04b05b09b3a52f9ed789a9f36073219a636
Minghui Ma <bright.ma@blackshark.com>
2018-03-26 15:15:51 +0800

    add aspect code for @Before and @After @AfterReturning @AfterThrowing
    Signed-off-by: Minghui Ma <bright.ma@blackshark.com>

---
+++ src/test/java/com/mamh/spring/demo/test/ArithmeticCalculatorTest.java
@@ -0,0 +1,24 @@
+package com.mamh.spring.demo.test;
+
+import com.mamh.spring.demo.aop.ArithmeticCalculator;
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+
+public class ArithmeticCalculatorTest {
+
+
+    @Test
+    public void test() {
+
+        ApplicationContext ctx = new ClassPathXmlApplicationContext("applicationContext.xml");
+
+        ArithmeticCalculator arithmeticCalculatorImpl = ctx.getBean(ArithmeticCalculator.class);
+        System.out.println("int test() " + arithmeticCalculatorImpl.add(3, 0));
+        System.out.println("\n");
+
+        System.out.println("int test() " + arithmeticCalculatorImpl.div(3, 0));
+    }
+
+}
