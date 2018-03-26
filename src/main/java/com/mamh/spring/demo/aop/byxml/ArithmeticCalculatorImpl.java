258d608da4642d00f7750fc29f490effc7994cf6
Minghui Ma <bright.ma@blackshark.com>
2018-03-26 16:44:30 +0800

    add package aop.byxml
    Signed-off-by: Minghui Ma <bright.ma@blackshark.com>

---
+++ src/main/java/com/mamh/spring/demo/aop/byxml/ArithmeticCalculatorImpl.java
@@ -0,0 +1,21 @@
+package com.mamh.spring.demo.aop.byxml;
+
+
+
+public class ArithmeticCalculatorImpl implements ArithmeticCalculator {
+    public int add(int i, int j) {
+        return i + j;
+    }
+
+    public int sub(int i, int j) {
+        return i - j;
+    }
+
+    public int mul(int i, int j) {
+        return i * j;
+    }
+
+    public int div(int i, int j) {
+        return i / j;
+    }
+}
--- src/test/java/com/mamh/spring/demo/test/ArithmeticCalculatorTest.java
+++ src/test/java/com/mamh/spring/demo/test/ArithmeticCalculatorTest.java
@@ -1,6 +1,6 @@
 package com.mamh.spring.demo.test;
 
-import com.mamh.spring.demo.aop.byannotation.ArithmeticCalculator;
+import com.mamh.spring.demo.aop.byxml.ArithmeticCalculator;
 import org.junit.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
@@ -18,7 +18,18 @@ public class ArithmeticCalculatorTest {
         System.out.println("int test() " + arithmeticCalculatorImpl.add(3, 0));
         System.out.println("\n");
 
-        System.out.println("int test() " + arithmeticCalculatorImpl.div(3, 0));
+        System.out.println("int test() " + arithmeticCalculatorImpl.div(3, 1));
     }
 
+    @Test
+    public void testByxml() {
+
+        ApplicationContext ctx = new ClassPathXmlApplicationContext("applicationContextByxml.xml");
+
+        ArithmeticCalculator arithmeticCalculatorImpl = (ArithmeticCalculator) ctx.getBean("arithmeticCalculator");
+        System.out.println("int test() " + arithmeticCalculatorImpl.add(3, 0));
+        System.out.println("\n");
+
+        System.out.println("int test() " + arithmeticCalculatorImpl.div(3, 1));
+    }
 }
