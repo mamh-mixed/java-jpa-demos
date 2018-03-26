f73c889e19a140c9d1a721b6ed30cb8cf7943315
Minghui Ma <bright.ma@blackshark.com>
2018-03-26 16:29:40 +0800

    use @Pointcut to declare jointpoint expression
    Signed-off-by: Minghui Ma <bright.ma@blackshark.com>

--- src/main/java/com/mamh/spring/demo/aop/ValidationAspect.java
+++ src/main/java/com/mamh/spring/demo/aop/ValidationAspect.java
@@ -11,7 +11,7 @@ import java.util.List;
 /**
  * 验证切面,切面要在ioc容器里面，所以加个@component注解
  * //把这个类声明为一个切面
- *
+ * <p>
  * 使用@Order（数字）来指定切面优先级，数字小越优先
  */
 @Order(1)
@@ -20,13 +20,21 @@ import java.util.List;
 public class ValidationAspect {
 
     /**
+     * 定义一个方法，用于声明切面表达式,一般的该方法中不需要其他代码
+     */
+    @Pointcut("execution(public int com.mamh.spring.demo.aop.ArithmeticCalculator.*(int,int))")
+    public void declareJointPointExpression() {
+
+    }
+
+    /**
      * 前置通知，在目标方法开始执行之前执行
      * <p>
      * 在 ArithmeticCalculator接口的每一个实现类的没一个方法之前都执行
      *
      * @param joinPoint
      */
-    @Before("execution(public int com.mamh.spring.demo.aop.ArithmeticCalculator.*(int,int))")
+    @Before(value = "declareJointPointExpression()")
     public void before(JoinPoint joinPoint) {
         String methodName = joinPoint.getSignature().getName();
         List<Object> args = Arrays.asList(joinPoint.getArgs());
@@ -39,7 +47,7 @@ public class ValidationAspect {
      *
      * @param joinPoint
      */
-    @After("execution(public int com.mamh.spring.demo.aop.ArithmeticCalculator.*(int,int))")
+    @After(value = "declareJointPointExpression()")
     public void after(JoinPoint joinPoint) {
         String methodName = joinPoint.getSignature().getName();
         List<Object> args = Arrays.asList(joinPoint.getArgs());
@@ -47,7 +55,7 @@ public class ValidationAspect {
     }
 
 
-    @AfterReturning(value = "execution(public int com.mamh.spring.demo.aop.ArithmeticCalculator.*(int,int))", returning = "result")
+    @AfterReturning(value = "declareJointPointExpression()", returning = "result")
     public void afterReturn(JoinPoint joinPoint, Object result) {
         String methodName = joinPoint.getSignature().getName();
         System.out.println("=ValidationAspect=afterReturn()............." + methodName + "   result: " + result);
@@ -58,7 +66,7 @@ public class ValidationAspect {
      * @param joinPoint
      * @param ex
      */
-    @AfterThrowing(value = "execution(public int com.mamh.spring.demo.aop.ArithmeticCalculator.*(int,int))", throwing = "ex")
+    @AfterThrowing(value = "declareJointPointExpression()", throwing = "ex")
     public void afterThrowing(JoinPoint joinPoint, Exception ex) {
         String methodName = joinPoint.getSignature().getName();
         System.out.println("=ValidationAspect=afterThrowing()..........." + methodName + " exception: " + ex);
