a21277129d4549ee508d2cba70071d5f778a9135
Minghui Ma <bright.ma@blackshark.com>
2018-05-02 15:46:30 +0800

    add @RequestMapping(value = /testMethod, method = RequestMethod.POST)
    Signed-off-by: Minghui Ma <bright.ma@blackshark.com>

--- src/main/java/com/mamh/springmvc/demo/handlers/HelloWorld.java
+++ src/main/java/com/mamh/springmvc/demo/handlers/HelloWorld.java
@@ -1,7 +1,9 @@
 package com.mamh.springmvc.demo.handlers;
 
+import jdk.internal.org.objectweb.asm.tree.analysis.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class HelloWorld {
@@ -10,6 +12,7 @@ public class HelloWorld {
     /**
      * 使用  RequestMapping 来映射请求的 url。
      * 返回值会通过视图解析器来解析为实际的视图。通过 prefix + returnValue + 后缀  得到路径，任何使用转发.
+     *
      * @return
      */
     @RequestMapping("/helloworld")
@@ -18,5 +21,14 @@ public class HelloWorld {
         System.out.println("hellow world...................");
 
         return "success";
+    }
+
+    @RequestMapping(value = "/testMethod", method = RequestMethod.POST)
+    private String testMethod() {
+        System.out.println("hellow world....testMethod...............");
+
+
+        return "success";
+
     }
 }
