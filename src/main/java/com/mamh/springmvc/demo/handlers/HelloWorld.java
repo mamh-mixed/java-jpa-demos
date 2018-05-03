72ef7268ae88d4d1257562a85bbeb22a90e30d49
Minghui Ma <bright.ma@blackshark.com>
2018-05-03 11:10:02 +0800

    add @RequestMapping HiddenHttpMethodFilter code
    Signed-off-by: Minghui Ma <bright.ma@blackshark.com>

--- src/main/java/com/mamh/springmvc/demo/handlers/HelloWorld.java
+++ src/main/java/com/mamh/springmvc/demo/handlers/HelloWorld.java
@@ -1,10 +1,11 @@
 package com.mamh.springmvc.demo.handlers;
 
-import jdk.internal.org.objectweb.asm.tree.analysis.Value;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+@RequestMapping("/springmvc")
 @Controller
 public class HelloWorld {
 
@@ -31,4 +32,50 @@ public class HelloWorld {
         return "success";
 
     }
+
+
+    @RequestMapping(value = "/testParams", params = {"username", "age!=10"},
+            headers = {"Accept-Language=zh-CN,en-US;q=0.7,en;q=0.3"})
+    private String testParams() {
+        System.out.println("hellow world....testParams...............");
+        return "success";
+    }
+
+    @RequestMapping(value = "/testPath/*/abc")
+    private String testPath() {
+        System.out.println("hellow world....testPath...............");
+        return "success";
+    }
+
+
+    @RequestMapping(value = "/testPathVariable{id}")
+    private String testPathVariable(@PathVariable(value = "id") int id) {
+        System.out.println("hellow world....PathVariable............... id = " + id);
+        return "success";
+    }
+
+    @RequestMapping(value = "/testREST/{id}", method = RequestMethod.GET)
+    private String testGET(@PathVariable(value = "id") int id) {
+        System.out.println("testREST  GET : " + id);
+        return "success";
+    }
+
+    @RequestMapping(value = "/testREST/{id}", method = RequestMethod.PUT)
+    private String testPUT(@PathVariable(value = "id") int id) {
+        System.out.println("testREST  PUT : " + id);
+        return "success";
+    }
+
+    @RequestMapping(value = "/testREST/{id}", method = RequestMethod.DELETE)
+    private String testDELETE(@PathVariable(value = "id") int id) {
+        System.out.println("testREST  DELETE : " + id);
+        return "success";
+    }
+
+    @RequestMapping(value = "/testREST", method = RequestMethod.POST)
+    private String testPOST() {
+        System.out.println("testREST   POST : ");
+        return "success";
+    }
+
 }
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -2,7 +2,28 @@
 <body>
 
 <a href="helloworld">helloworld</a>
+<br><br><br><br><br><br>
 
 
+
+<form action="springmvc/testREST/1" method="post">
+    <input type="hidden" name="_method" value="PUT"/>
+    <input type="submit" value="TestRest PUT"/>
+</form>
+<br><br>
+
+<form action="springmvc/testREST/1" method="post">
+    <input type="hidden" name="_method" value="DELETE"/>
+    <input type="submit" value="TestRest DELETE"/>
+</form>
+<br><br>
+
+<form action="springmvc/testREST" method="post">
+    <input type="submit" value="TestRest POST"/>
+</form>
+<br><br>
+
+<a href="springmvc/testREST/1">Test Rest Get</a>
+<br><br>
 </body>
 </html>
