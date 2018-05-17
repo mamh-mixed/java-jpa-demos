973355119b53219979cf570fb4de6c5b37c6132e
Minghui Ma <bright.ma@blackshark.com>
2018-05-17 09:57:35 +0800

    add @ResponseBody test code
    Signed-off-by: Minghui Ma <bright.ma@blackshark.com>

--- src/main/java/com/mamh/springmvc/demo/handlers/EmployeeHandler.java
+++ src/main/java/com/mamh/springmvc/demo/handlers/EmployeeHandler.java
@@ -6,14 +6,14 @@ import com.mamh.springmvc.demo.entities.Employee;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.WebDataBinder;
-import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.Collection;
 import java.util.Map;
 
 @Controller
@@ -25,6 +25,13 @@ public class EmployeeHandler {
     @Autowired
     private DepartmentDao departmentDao;
 
+    @ResponseBody
+    @RequestMapping("/testJson")
+    public Collection<Employee> testJson( ) {
+        System.out.println("testJson......." );
+
+        return  employeeDao.getAll();
+    }
 
     @RequestMapping("/testConverter")
     public String testConverter(@RequestParam("employee") Employee employee) {
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -1,10 +1,34 @@
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
 
 <html>
-<body>
-<a href="/emps">List All Employees</a>
-<br><br>
+<head>
+    <script type="text/javascript" src="scripts/jquery-1.9.1.min.js"></script>
+    <script type="text/javascript">
+        $(function () {
+            $("#testJson").click(function () {
+                var url = this.href;
+                var args = {};
+                alert("url: " + url);
+                $.post(url, args, function (data) {
+                    for (var i = 0; i < data.length; i++) {
+                        var id = data[i].id;
+                        var lastname = data[i].lastName;
 
+                        alert(id + " : " + lastname);
+                    }
+
+                });
+
+                return false;
+            });
+
+        })
+    </script>
+</head>
+<body>
+
+
+<a href="/testJson" id="testJson">test json</a>
 
 
 </body>
