3328dbec5b7b9b2a14f0efcc06bbfdcb960dc805
Minghui Ma <bright.ma@blackshark.com>
2018-05-30 15:16:22 +0800

    add hello world test code for jpa
    Signed-off-by: Minghui Ma <bright.ma@blackshark.com>

---
+++ src/main/java/com/mamh/jpa/Customer.java
@@ -0,0 +1,84 @@
+package com.mamh.jpa;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.persistence.Transient;
+import java.util.Date;
+
+@Table(name = "jpa_customer")
+@Entity
+public class Customer {
+    private Integer id;
+    private String lastName;
+    private String email;
+    private int age;
+
+    private Date createTime;
+    private Date birth;
+
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    @Column(name = "last_name")
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Transient
+    public String getInfo() {
+        return super.toString();
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    @Temporal(TemporalType.TIMESTAMP)
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    @Temporal(TemporalType.DATE)
+    public Date getBirth() {
+        return birth;
+    }
+
+    public void setBirth(Date birth) {
+        this.birth = birth;
+    }
+}
