ecc29167501a6e0fac5c22cb06a8c2b3f0893096
mamh <bright.ma@blackshark.com>
2018-07-04 16:12:10 +0800

    add spring data jpa @Query annotation test code
    Signed-off-by: mamh <bright.ma@blackshark.com>

--- src/main/java/com/mamh/jpa/springdata/PeopleRepository.java
+++ src/main/java/com/mamh/jpa/springdata/PeopleRepository.java
@@ -1,6 +1,8 @@
 package com.mamh.jpa.springdata;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.Repository;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Date;
 import java.util.List;
@@ -25,4 +27,23 @@ public interface PeopleRepository extends Repository<People, Integer> {
 
     //where email in (?, ?, ?)  or birth < ?
     List<People> getByEmailInOrBirthLessThan(List<String> emails, Date birth);
+
+
+    //查询id最大的people
+    @Query("select p from People p where p.id = (select max(p2.id) from People p2)")
+    People getMaxIdPeople();
+
+
+    @Query("select p from People p where p.lastName = ?1 and p.email = ?2")
+    List<People> getPeople(String last, String email);
+
+    @Query("select p from People p where p.lastName = :lastName and p.email = :email")
+    List<People> getPeople1(@Param("lastName") String last, @Param("email") String email);
+
+    @Query("select p from People p where p.lastName like %:lastName% and p.email like %:email%")
+    List<People> getPeople2(@Param("lastName") String last, @Param("email") String email);
+
+
+    @Query(value = "select count(id) from  jpa_people",nativeQuery = true)
+    Long getTotal();
 }
--- src/test/java/com/mamh/jpa/SpringDataTest.java
+++ src/test/java/com/mamh/jpa/SpringDataTest.java
@@ -8,21 +8,38 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import javax.sql.DataSource;
+import java.util.List;
 
 public class SpringDataTest {
 
     private ApplicationContext context = null;
+    private PeopleRepository peopleRepository;
 
     @Before
     public void init() {
         context = new ClassPathXmlApplicationContext("spring.xml");
+        peopleRepository = context.getBean(PeopleRepository.class);
     }
 
+
+    @Test
+    public void testQueryAnnotation(){
+        People people = peopleRepository.getMaxIdPeople();
+        System.out.println(people);
+
+        List<People> people1 = peopleRepository.getPeople("aa", "aa");
+        System.out.println(people1);
+
+        List<People> people2 = peopleRepository.getPeople2("a", "a");//模糊查询
+        System.out.println(people2);
+
+        Long total = peopleRepository.getTotal();//本地sql查询
+        System.out.println(total);
+    }
 
 
     @Test
     public void test(){
-        PeopleRepository peopleRepository = context.getBean(PeopleRepository.class);
         People people = peopleRepository.getByLastName("aa");
         System.out.println(people);
 
